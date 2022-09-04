cbb51c45ebae8a5a8bab8c1c13e6b06f0df4b409
marinamyazina <marinasochi1@rambler.ru>
2022-09-05 00:19:31 +0300

    Выполнили Exercise002_Distance

---
+++ Exercise002_Distance/Program.cs
@@ -0,0 +1,44 @@
+﻿static void emptyString(int howmanyString)        // Выводит нужное кол-во пустых строк
+{
+    int a = 0;
+    while (a < howmanyString)
+    {
+        Console.WriteLine();
+        a++;
+    }
+}
+
+emptyString(6);
+string text = new String('*', 40);         // Выводит заданное количество символов,
+                                           //  в данном случае 40
+Console.WriteLine("    Задача 2");
+Console.WriteLine(text);
+emptyString(1);
+Console.WriteLine("  Напишите программу, которая принимает \r\nна вход координаты двух точек и \r\nнаходит расстояние между ними в 3D пространстве.");
+emptyString(1);
+static void Distance3D()
+{
+    try
+    {
+        Console.WriteLine("  Введите координаты первой точки: ");
+        double x1 = double.Parse(Console.ReadLine());
+        double y1 = double.Parse(Console.ReadLine());
+        double z1 = double.Parse(Console.ReadLine());
+        Console.WriteLine("  Введите координаты второй точки: ");
+        double x2 = double.Parse(Console.ReadLine());
+        double y2 = double.Parse(Console.ReadLine());
+        double z2 = double.Parse(Console.ReadLine());
+        double distance = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
+        Console.WriteLine($"Расстояние между данными точками: {Math.Round(distance, 2)}");
+    }
+
+    catch (FormatException)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Это не число!"); Console.ResetColor();
+        Distance3D();
+    }
+}
+Distance3D();
+emptyString(6);
+Console.WriteLine(text);
